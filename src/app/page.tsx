e6f2a66aae276d5dc2e94c5fe5f49c7e3c805067
XimenaLH21 <ximena@dicalzeus.com>
2026-01-09 11:29:28 -0700

    feat: Integrate HeroSection into Home component for improved landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import AboutSection from "@/components/Landing/AboutSection";
 import FooterSection from "@/components/Landing/FooterSection";
 import HeaderSection from "@/components/Landing/HeaderSection";
+import HeroSection from "@/components/Landing/HeroSection";
 import HowWorkSection from "@/components/Landing/HowWorkSection";
 import LocationContactSection from "@/components/Landing/LocationContactSection";
 import ProductServicesSection from "@/components/Landing/ProductServicesSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen border border-red-500">
+    <div className="flex flex-col items-center h-screen">
       <HeaderSection />
+      <HeroSection />
       <AboutSection />
       <HowWorkSection />
       <ProductServicesSection />
