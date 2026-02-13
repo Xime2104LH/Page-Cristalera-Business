209f03fff869b69c5984fb247429d7a301711edc
XimenaLH21 <ximena@dicalzeus.com>
2026-02-13 09:30:24 -0700

    feat: changes to responsive design

--- src/components/Landing/HeroSection.tsx
+++ src/components/Landing/HeroSection.tsx
@@ -4,22 +4,22 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div className="w-full  hero-section-background flex border border-red-600 ">
-      <div className="p-4 md:p-16 lg:w-1/2 lg:p-10 xl:p-20 2xl:px-50 2xl:py-20">
+    <div className="w-full hero-section-background flex mt-[70px] 2xl:px-30">
+      <div className="px-4 py-16 md:p-16 lg:w-1/2 lg:p-10 xl:p-20  2xl:py-20 2xl:w-2/3 ">
         <div className="text-blue-primary text-sm bg-blue-light/20 py-1 px-4 rounded-full w-fit flex items-center gap-2">
           <span className=" inline-block size-2 rounded-full bg-blue-secondary animate-pulse"></span>
           Más de 20 años de experiencia
         </div>
 
         <h1 className="text-4xl lg:text-[56px] xl:text-7xl xl:leading-tight font-bold my-8">Transformamos tu espacio con <span className="text-blue-primary">vidrio y aluminio</span> de calidad</h1>
-        <p className="text-gray-500 mb-8 xl:text-2xl">Somos especialistas en la elaboración de ventanas, closets, canceles de baño, ventanales y más. Diseños personalizados para cada necesidad</p>
+        <p className="leading-9 text-gray-500 mb-8 text-2xl xl:text-2xl">Somos especialistas en la elaboración de ventanas, closets, canceles de baño, ventanales y más. Diseños personalizados para cada necesidad</p>
 
         <div className="md:flex md:items-center md:gap-4">
-          <Button variant="default" size="lg" className="w-full mb-4 font-normal md:w-auto">
+          <Button variant="default" size="lg" className="w-full mb-4 text-[12px] font-normal md:w-auto">
             Ver Catálogo
             <ArrowRight className="size-4" />
           </Button>
-          <Button variant="outline" size="lg" className="w-full mb-4 font-normal md:w-auto">
+          <Button variant="outline" size="lg" className="w-full mb-4 text-[12px] font-normal md:w-auto">
             <Phone className="size-4" />
             Llamar ahora
           </Button>
@@ -36,8 +36,8 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-      <div className="hidden lg:flex lg:items-center lg:justify-center lg:p-10 w-1/2">
-        <div className="relative w-125">
+      <div className="hidden lg:flex lg:items-center lg:justify-center lg:p-10 w-1/2 2xl:w-1/3">
+        <div className="relative w-125 z-1">
           <div className="absolute top-21.5 -left-11.75 flex flex-col bg-white rounded-2xl p-4">
             <span className="text-blue-primary text-base">Ventanas</span>
             <span className="text-gray-500 text-sm">Diseño personalizado</span>
