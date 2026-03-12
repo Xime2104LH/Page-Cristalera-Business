09e19fca45d488b454a82545ef7c4a424e3123c2
XimenaLH21 <ximena@dicalzeus.com>
2026-03-12 13:27:23 -0700

    feat: it remove all in how work section. It needs works yet

--- src/components/Landing/HowWorkSection.tsx
+++ src/components/Landing/HowWorkSection.tsx
@@ -1,29 +1,9 @@
-import ImageProduct from '@/components/General/imageProduct'
-
-const products = [
-  {
-    imageUrl: "/trabajo-ventanas-proyecto-2.jpeg",
-    title: "Ventanas",
-    description: "Ventanas de aluminio corredizas, abatibles y proyectantes."
-  }
-]
 
 const HowWorkSection = () => { 
   return (
-    <div className="text-center p-8">
-      <div>
-        <h3 className="text-blue-light text-2xl mb-6">Nuestros productos</h3>
-        <h1 className="text-4xl font-bold mb-6">Catálogo de productos y servicios</h1>
-        <p className="text-2xl text-gray-500">Descubre nuestra amplia gama de productos diseñados para transformar y mejorar cada espacio de tu hogar.</p>
-      </div>
-      <div>
-        {
-          products.map(item => (
-            <ImageProduct key={item.title} title={item.title} imageUrl={item.imageUrl} description={item.description} />
-          ))
-        }
-      </div>
-    </div>
+   <div>
+    Cómo trabajamos
+   </div>
   ) 
 };
 
