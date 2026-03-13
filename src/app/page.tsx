ff98c3256870f02049b50d58c864827d3a0e7ec9
XimenaLH21 <ximena@dicalzeus.com>
2026-03-12 18:10:38 -0700

    fix: change the order of the component correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <HeaderSection />
       <HeroSection />
       <AboutSection />
-      <HowWorkSection />
       <ProductServicesSection />
+      <HowWorkSection />
       <LocationContactSection />
       <FooterSection />
     </div>
