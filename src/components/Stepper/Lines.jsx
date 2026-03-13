c71faab33878e82f81dccd243b6d08eb7fec7b4c
XimenaLH21 <ximena@dicalzeus.com>
2026-03-12 18:08:51 -0700

    feat: steppers line of the HowWorkSection

---
+++ src/components/Stepper/Lines.jsx
@@ -0,0 +1,12 @@
+
+
+export const Lines = () => {
+  return (
+    <div className='relative flex flex-row lg:bottom-65 lg:left-38 lg:gap-27 xl:left-51 xl:bottom-60 xl:gap-25 2xl:left-90 2xl:gap-27'>
+      <hr className='border border-blue-secondary xl:xl:w-60 lg:w-30 2xl:w-132'/>
+      <hr className='border border-blue-secondary xl:w-60 lg:w-30 2xl:w-132'/>
+      <hr className='border border-blue-secondary xl:w-60 lg:w-30 2xl:w-132'/>
+      <hr className='border border-blue-secondary xl:w-60 lg:w-30 2xl:w-132'/>
+    </div>
+  )
+}
