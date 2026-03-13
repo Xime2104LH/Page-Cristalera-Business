8997412c3847a1541bc695cf58815d735fb5c36b
XimenaLH21 <ximena@dicalzeus.com>
2026-03-12 18:04:50 -0700

    feat: new component header section but with white title

---
+++ src/components/General/HeaderSectionsWhite.tsx
@@ -0,0 +1,11 @@
+
+
+export const HeaderSectionsWhite = () => {
+  return ( 
+    <div>
+      <h3 className="text-blue-light text-2xl mb-6">Proceso de trabajo</h3>
+      <h1 className="text-4xl font-bold mb-6 text-white">¿Cómo trabajamos?</h1>
+      <p className="text-2xl text-gray-300">Un proceso simple y transparente para que obtengas exactamente lo que necesitas.</p>
+    </div>
+  )
+}
