3b6761d3ed969d0f2ee8d65f4e473ddd45e8600a
XimenaLH21 <ximena@dicalzeus.com>
2026-03-12 18:06:56 -0700

    refactor: implement the HeaderSection component

--- src/components/Landing/ProductServicesSection.tsx
+++ src/components/Landing/ProductServicesSection.tsx
@@ -1,4 +1,5 @@
 import ImageProduct from '@/components/General/ImageProduct'
+import { HeaderSections } from '../General/HeaderSections';
 
 const products = [
   {
@@ -40,12 +41,12 @@ const products = [
 
 const ProductServicesSection = () => { 
   return (
-    <div className="text-center p-8 lg:px-28">
-      <div>
-        <h3 className="text-blue-light text-2xl mb-6">Nuestros productos</h3>
-        <h1 className="text-4xl font-bold mb-6">Catálogo de productos y servicios</h1>
-        <p className="text-2xl text-gray-500">Descubre nuestra amplia gama de productos diseñados para transformar y mejorar cada espacio de tu hogar.</p>
-      </div>
+    <div className="text-center p-8 md:pt-20 lg:px-28 lg:py-16">
+      <HeaderSections 
+        wordKey='Nuestros productos' 
+        title='Catálogo de productos y servicios'
+        description='Descubre nuestra amplia gama de productos diseñados para transformar y mejorar cada espacio de tu hogar.'
+      />
       <div className="mt-12 md:grid md:grid-cols-2 md:gap-8 lg:grid-cols-3  xl:grid-cols-4 2xl:grid-cols-5 2xl:w-[2000px]">
         {
           products.map(item => (
