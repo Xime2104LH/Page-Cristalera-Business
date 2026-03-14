b1ab9ffb5262199383b0ae2afd778083eddf8a7a
XimenaLH21 <ximena@dicalzeus.com>
2026-03-13 18:44:42 -0700

    feat(contactsection): Location Contact Section was done with a responsive design

--- src/components/Landing/LocationContactSection.tsx
+++ src/components/Landing/LocationContactSection.tsx
@@ -1,9 +1,15 @@
-
+import { HeaderSections } from "../General/HeaderSections";
+import { ContainerContact } from "../ContainerContact/ContainerContact";
 
 const LocationContactSection = () => { 
   return (
-    <div>
-      <h1>Location Contact Section</h1>
+    <div className="p-8 py-32 text-center bg-blue-primary/1 w-full lg:px-24 flex flex-col items-center">
+      <HeaderSections 
+        wordKey="Contacto"
+        title="¿Listo para transformar tu espacio?"
+        description="Contáctanos hoy mismo. Estamos listos para ayudarte."
+      />
+      <ContainerContact />
     </div>
   ) 
 };
